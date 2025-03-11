2f82045f71c363822b850da3ba75038fa53d8419
Nathan <nathan.sujatno@gmail.com>
2025-03-10 19:35:42 -0500

    added verify email and forgot password

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,7 +1,14 @@
 const User = require('../model/user')
-const { sendError } = require('../utils/helper')
+const VerificationToken = require('../model/verificationToken')
+const ResetToken = require('../model/resetToken')
+const { sendError, createRandomBytes } = require('../utils/helper')
+
+const crypto = require('crypto')
 
 const jwt = require('jsonwebtoken')
+const { generateOTP, mailTransport } = require('../utils/mail')
+const { isValidObjectId } = require('mongoose')
+const verificationToken = require('../model/verificationToken')
 
 exports.createUser = async (req, res) =>{
     const {name, email, password} = req.body
@@ -14,7 +21,22 @@ exports.createUser = async (req, res) =>{
         password 
     })
 
+    const OTP = generateOTP()
+    const verificationToken = new VerificationToken({
+        owner: newUser._id,
+        token: OTP
+    })
+
+    await verificationToken.save()
     await newUser.save()
+
+    mailTransport().sendMail({
+        from: 'emailverification@email.com',
+        to: newUser.email,
+        subject: "Verify your email account",
+        html: `<h1>Your verification code is: ${OTP}</h1>`
+    })
+
     res.send(newUser)
 }
 
@@ -32,5 +54,90 @@ exports.signin = async (req, res) => {
         expiresIn: '1d'
     })
 
-    res.json({success: true, user: {name: user.name, email: user.email, id: user._id, token}})
+    res.json({success: true, message: "sign in successful", user: {name: user.name, email: user.email, id: user._id, token}})
+}
+
+exports.verifyEmail = async (req, res) => {
+    const {userId, otp} = req.body
+    if(!userId || !otp.trim()) return sendError(res, "Invalid request, missing parameters!");
+
+    if(!isValidObjectId(userId)) return sendError(res, "Invalid user id!");
+
+    const user = await User.findById(userId)
+    if(!user) return sendError(res, "User not found!");
+
+    if(user.verified) return sendError(res, "This account is already verified!");
+
+    const token = await VerificationToken.findOne({owner: user._id})
+    if(!token) return sendError(res, "Sorry, user not found");
+
+    const isMatched = await token.compareToken(otp)
+    if(!isMatched) return sendError(res, "Please provide a valid token!");
+
+    user.verified = true;
+
+    await VerificationToken.findByIdAndDelete(token._id);
+    await user.save();
+
+    mailTransport().sendMail({
+        from: 'emailverification@email.com',
+        to: user.email,
+        subject: "Verify your email account",
+        html: `<h1>Email verified successfully! Thank you for connecting with us!</h1>`
+    })
+    res.json({success: true, message: "your email is verified!", user: {name: user.name,
+        email: user.email, id: user._id
+    }})
+
+}
+
+exports.forgotPassword = async (req, res) => {
+    const {email} = req.body;
+    if(!email) return sendError(res, 'Please provide a valid email!')
+
+    const user = await User.findOne({email})
+    if(!user) return sendError(res, 'User not found!')
+
+    const token = await ResetToken.findOne({owner: user._id})
+    if(token) return sendError(res, 'Only after one hour you can request for another tooken!!')
+
+    const randomBytes = await createRandomBytes()
+    const resetToken = new ResetToken({owner: user._id, token: randomBytes})
+    await resetToken.save()
+
+    mailTransport().sendMail({
+        from: 'security@email.com',
+        to: user.email,
+        subject: "Password Reset",
+        html: `<a href="${`http://localhost:3000/reset-password?token=${randomBytes}&id=${user._id}`}">Password reset link</a>`
+    })
+
+    res.json({success: true, message: 'Password reset link is sent to your email.'})
+}
+
+exports.resetPassword = async (req, res) => {
+    const {password} = req.body
+
+    const user = await User.findById(req.user._id);
+    if(!user) return sendError(res, 'user not found!');
+
+    const isSamePassword = await user.comparePassword(password)
+    if(isSamePassword) return sendError(res, 'New password must be different!');
+
+    if(password.trim().length < 8 || password.trim().length > 20)
+        return sendError(res, 'Password must be 8 to 20 characters long!');
+
+    user.password = password.trim();
+    await user.save();
+
+    await ResetToken.findOneAndDelete({owner: user._id})
+
+    mailTransport().sendMail({
+        from: 'security@email.com',
+        to: user.email,
+        subject: "Password Reset Successfully",
+        html: `<h1>Password was reset successfully! Now you can login with your new password!</h1>`
+    })
+
+    res.json({success: true, message: "Password Reset Successfully"})
 }
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,11 +1,15 @@
 const router = require('express').Router()
 
-const { createUser, signin } = require('../controllers/user');
+const { createUser, signin, forgotPassword, verifyEmail, resetPassword } = require('../controllers/user');
+const { isResetTokenValid } = require('../middlewares/user');
 const { validateUser, validate } = require('../middlewares/validator');
 
 
 router.post('/create', validateUser, validate, createUser);
 router.post('/signin', signin);
+router.post('/verify-email', verifyEmail);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password", isResetTokenValid, resetPassword);
 
 
 
