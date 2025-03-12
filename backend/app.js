414871eaaa23b6124267e8489b91a4ba2a74c5ed
Nathan <nathan.sujatno@gmail.com>
2025-03-12 11:12:39 -0500

    added cors and disabled email verification temporarily

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 require('dotenv').config();
 require('./db')
+const cors = require('cors');
 
 const userRouter = require('./routes/user')
 
@@ -8,8 +9,10 @@ const app = express()
 
 const PORT = process.env.PORT || 8000
 
+app.use(cors());
 app.use(express.json()) // This is used to take the data given and store it in the req.body which is why we can reference it later on
 app.use('/api/user', userRouter);
+
 
 app.listen(PORT, () => {
     console.log(`App is running on port ${PORT}`)
