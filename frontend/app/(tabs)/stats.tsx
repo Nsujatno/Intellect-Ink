643ecbf3056ec207f638e6eb833fc2d87f538304
afraibnat <afraibnat91@gmail.com>
2025-03-18 12:51:07 -0500

    editing layout of stats page

--- frontend/app/(tabs)/stats.tsx
+++ frontend/app/(tabs)/stats.tsx
@@ -24,12 +24,7 @@ export default function Stats() {
   useEffect(() => {
     const fetchStats = async () => {
       try {
-        const userID = ""; // userID placeholder
-        const response = await axios.get(`https://your-api-url.com/${userId}`); // API link to fetch user stats
-        const timeSpent = response.data;
-
-        // Map API data
-        const formattedData = timeSpent.map((dayData, index) => ({
+        /* const formattedData = timeSpent.map((dayData, index) => ({
           day: chartData[index].day, // Days of the week
           books: dayData.books,
           poems: dayData.poems,
@@ -37,18 +32,19 @@ export default function Stats() {
           research: dayData.research,
         }));
 
-        setChartData(formattedData); // Update chart state data with formatted
+        setChartData(formattedData);
 
-        // Getting today's data
+        Getting today's data
         const daysOfTheWeek = ["Sun", "M", "T", "W", "Th", "F", "S"];
         const currentDay = daysOfTheWeek[new Date().getDay()];
         const currentDayData = formattedData.find((day) => day.day === currentDay);
 
-        // Update state with today's data, defaulting to zeros if no data is found
+        Update state with today's data, defaulting to zeros if no data is found
         setTodayData(currentDayData || { books: 0, poems: 0, politics: 0, research: 0 });
+        */
 
       } catch (error) {
-        console.error("Error fetching data:", error)
+        // console.error("Error fetching data:", error)
       } finally {
         setLoading(false);
       }
@@ -80,8 +76,8 @@ export default function Stats() {
             domainPadding={{ left: 50, right: 50, top: 30 }}
             domain={{ y: [0, 400] }}
             axisOptions={{
-              tickValuesX: ["Sun", "M", "T", "W", "Th", "F", "S"], // Days on x-axis
-              tickValuesY: [0, 100, 200, 300, 400], // Labels on y-axis
+              // tickValuesX: ["Sun", "M", "T", "W", "Th", "F", "S"], // Days on x-axis
+              // tickValuesY: [0, 100, 200, 300, 400], // Labels on y-axis
               formatXLabel: (value) => value,
               formatYLabel: (value) => `${value}`,
               lineColor: "#000",
@@ -93,7 +89,7 @@ export default function Stats() {
               <StackedBar
                 chartBounds={chartBounds}
                 points={[points.books, points.poems, points.politics, points.research]}
-                colors={["#4E86E9", "#0A0B78", "#5A5CF6", "#3335CF"]}
+                colors={["#4E86E9", "#0A0B78", "#5A5CF6", "#3335CF"]} // Categories and corresponding colors
                 barOptions={( { isBottom, isTop }) => {
                   return {
                     roundedCorners: isTop ? { topLeft: 10, topRight: 10, }
@@ -109,54 +105,57 @@ export default function Stats() {
 
 
         {/* Statistics Category Time Spent Box */}
-        <Text style={[textStyles.heading1, {marginTop: 150, alignSelf: "center"}]}>Time Spent</Text>
+        <Text style={[textStyles.heading1, {marginTop: 130, alignSelf: 'center'}]}>Time Spent</Text>
+        <Text style={textStyles.heading2}>Per Category</Text>
         <View style={styles.timeSpentContainer}>
           <Image
               source={require('../../assets/images/stats_box1.png')}
-              style={styles.statsBox1Image}
+              style={styles.statsBoxPlacement1}
           />
           {/* View Todays Data */}
           <View style={styles.todaysDataContainer}>
             <View style={styles.categoryRow}>
               <Text style={styles.categoryName}>Books</Text>
-              <Text style={styles.categoryTime}>{todayData?.books ?? 0}</Text>
+              {/* <Text style={styles.categoryTime}>{todayData?.books ?? 0}</Text> */}
             </View>
           <View style={styles.categoryRow}>
               <Text style={styles.categoryName}>Poems</Text>
-              <Text style={styles.categoryTime}>{todayData?.poems ?? 0}</Text>
+              {/* <Text style={styles.categoryTime}>{todayData?.poems ?? 0}</Text> */}
           </View>
           <View style={styles.categoryRow}>
               <Text style={styles.categoryName}>Politics</Text>
-              <Text style={styles.categoryTime}>{todayData?.politics ?? 0}</Text>
+              {/* <Text style={styles.categoryTime}>{todayData?.politics ?? 0}</Text> */}
           </View>
           <View style={styles.categoryRow}>
               <Text style={styles.categoryName}>Research</Text>
-              <Text style={styles.categoryTime}>{todayData?.research ?? 0}</Text>
+              {/* <Text style={styles.categoryTime}>{todayData?.research ?? 0}</Text> */}
           </View>
         </View>
 
 
         {/* Statistics Overall Box */}
-        <Text style={[textStyles.heading1, {marginTop: 100, alignSelf: "center"}]}>Statistics</Text>
+        <Text style={[textStyles.heading1, {marginTop: -25, marginBottom: 30, alignSelf: 'center'}]}>Statistics</Text>
         <View style={styles.overallStatsContainer}>
           <Image
-            source={require('../../assets/images/stats_box2.png')}
-            style={styles.statsBox2Image}
+            source={require('../../assets/images/stats_box1.png')}
+            style={styles.statsBoxPlacement2}
           />
         </View>
 
 
         {/* Achievement & Leaderboard Buttons */}
+        <View style={{flexDirection: 'column', marginTop: 130, width: 300, height: 60}}>
         <Buttons
             title=' Achievements '
             variant='purple'
-            onPress={() => router.push('/login')} // Change to dropdown
+            onPress={() => router.push('/home')}
         />
         <Buttons
             title=' Leaderboard '
             variant='purple'
-            onPress={() => router.push('/login')} // Change to dropdown
+            onPress={() => router.push('/home')}
         />
+        </View>
       </View>
     </View>
   </ScrollView>
@@ -168,20 +167,20 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   imagebg: { // Background Image
-    resizeMode: 'contain',
+    resizeMode: 'cover',
     width: '100%',
     height: undefined,
     aspectRatio: 0.275,
   },
   imageContainer: {
     width: '100%',
-    height: '60%',
     marginTop: 130,
   },
   textContainer: {
-    position: "absolute",
-    top: 50,
+    marginTop: 50,
+    position: 'absolute',
     alignSelf: 'center',
+    alignItems: 'center',
   },
   chartContainer: { // Statistics Bar Graph
     backgroundColor: "#FFFFFF",
@@ -197,48 +196,42 @@ const styles = StyleSheet.create({
     top: 70,
   },
   timeSpentContainer: { // Statistics Category Time Spent Box
-    top: 40,
-    position: "relative",
     alignItems: "center",
-    marginTop: 20,
-    width: 300,
-    height: 180,
-    padding: 15,
   },
   overallStatsContainer: { // Statistics Overall Box
-    top: 0,
     position: "relative",
     alignItems: "center",
-    width: 300,
     height: 180,
   },
-  statsBox1Image: {
-    width: 500,
-    height: 180,
+  statsBoxPlacement1: {
+    width: 300,
+    height: 400,
     resizeMode: "contain",
     alignSelf: "center",
+    marginTop: -80,
   },
-  statsBox2Image: {
+  statsBoxPlacement2: {
     width: 480,
     height: 200,
     resizeMode: "contain",
     alignSelf: "center",
   },
   todaysDataContainer: { // Data for Statistics Category Time Spent Box
-    position: "absolute",
-    left: 0,
+    position: "absolute", // overlay image
+    top: 25,
+    left: 50, // adjust once color boxes & times are added
     right: 0,
-    top: 0,
-    bottom: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
     padding: 15,
   },
   categoryRow: {
     flexDirection: "row",
     justifyContent: "space-between",
-    alignItems: "center",
-    paddingVertical: 5,
+    width: '80%',
+    paddingVertical: 8,
   },
-  categoryName: {
+  categoryName: { // Adjust fonts to textstyles
     fontSize: 16,
     fontWeight: "500",
     color: "#555",
