3b56ac35fe1248776e88d5b71f266179a3331ae5
Nathan <nathan.sujatno@gmail.com>
2025-03-28 18:54:55 -0500

    integartion with backend for searching

--- frontend/app/(tabs)/search.tsx
+++ frontend/app/(tabs)/search.tsx
@@ -1,10 +1,62 @@
 import { Text, View, SafeAreaView, TextInput, StyleSheet} from "react-native";
 import { useState } from "react";
+import axios from 'axios'
 
 export default function Search() {
     const [searchQuery, setSearchQuery] = useState("");
-    const handleSearch = (query: string) => {
+    const handleSearch = async (query: string) => {
         setSearchQuery(query);
+        console.log(query);
+
+        try{
+            const response = await axios.post("http://localhost:8000/api/article/search", {keyword: query});
+            for(let i = 0; i < response.data.length; i++){
+                console.log(response.data[i])
+            }
+        }catch(error){
+            if (axios.isAxiosError(error)) {
+                if(error.response){
+                    console.log('Error: ', error.response.data)
+                }
+            }
+        }
+        try{
+            const response = await axios.post("http://localhost:8000/api/book/search", {keyword: query});
+            for(let i = 0; i < response.data.length; i++){
+                console.log(response.data[i])
+            }
+        }catch(error){
+            if (axios.isAxiosError(error)) {
+                if(error.response){
+                    console.log('Error: ', error.response.data)
+                }
+            }
+        }
+        try{
+            const response = await axios.post("http://localhost:8000/api/news/search", {keyword: query});
+            for(let i = 0; i < response.data.length; i++){
+                console.log(response.data[i])
+            }
+        }catch(error){
+            if (axios.isAxiosError(error)) {
+                if(error.response){
+                    console.log('Error: ', error.response.data)
+                }
+            }
+        }
+        try{
+            const response = await axios.post("http://localhost:8000/api/poem/search", {keyword: query});
+            for(let i = 0; i < response.data.length; i++){
+                console.log(response.data[i])
+            }
+        }catch(error){
+            if (axios.isAxiosError(error)) {
+                if(error.response){
+                    console.log('Error: ', error.response.data)
+                }
+            }
+        }
+
     }
 
     return (
