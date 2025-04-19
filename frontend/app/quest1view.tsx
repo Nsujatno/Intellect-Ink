a220214024b10a4b381a9dba0e638f5242763d89
afraibnat <afraibnat91@gmail.com>
2025-04-19 15:46:05 -0500

    change in predifined reply

--- frontend/app/quest1view.tsx
+++ frontend/app/quest1view.tsx
@@ -25,7 +25,7 @@ export default function question1View() {
             {
                 _id: '2',
                 name: 'Name',
-                text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+                text: 'Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.'
             },
         ];
 
